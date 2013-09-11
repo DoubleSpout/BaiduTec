1f03f65c9963c8d9bd96b85457cb8f311381d4ea
snoopyxdy <snoopyxdy@gmail.com>
2013-09-11 21:32:34 +0800

    pool bugs

--- ifile/ifile.js
+++ ifile/ifile.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var ifile = require("ifile");
-app.use(ifile.connect([["/static",__dirname]])); 
+app.use(ifile.connect()); 
+console.log(__dirname)
 //default is [['/static',__dirname]];	
 app.listen(3000);
